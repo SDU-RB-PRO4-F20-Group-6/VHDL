3f355b8a60a7c1cd2dc460c71c23174821c4c328
Claus <clhuy18@student.sdu.dk>
2020-03-27 14:45:07 +0100

    add debouncing

--- claus_quadrature_decoder/claus_quadrature_decoder.srcs/sources_1/new/quadratur_decoder.vhd
+++ claus_quadrature_decoder/claus_quadrature_decoder.srcs/sources_1/new/quadratur_decoder.vhd
@@ -21,7 +21,9 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.numeric_std.ALL;
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -46,40 +48,41 @@ signal position    : unsigned(15 downto 0) := (others => '0');
 begin
     
     process (clk)
-    variable delay: integer range 0 to 100000;
+    variable delay: integer range 0 to 1000000;
     begin
-    if rising_edge(clk) then
-        if delay < 100000 then
-            delay := delay +1;
-        else
-            case((preposition & JB)) is
-                when "0111" =>
-                    position <= position + 1;
-                when "1110" =>
-                    position <= position + 1;
-                when "1000" =>
-                    position <= position + 1;
-                when "0001" =>
-                    position <= position + 1;
-                when "0100" =>
-                    position <= position - 1;
-                when "0010" =>
-                    position <= position - 1;
-                when "1011" =>
-                    position <= position - 1;
-                when "1101" =>
-                    position <= position - 1;
-                when others =>
-                    position <= position;
-                end case;
-                preposition <= JB;
-                led <= std_logic_vector(position);
-          end if;
-          
-          if(btnC = '1') then
-            position <= (others => '0');
-          end if;
-          
-      end if;
+        if rising_edge(clk) then
+            delay := delay + 1;
+            if (delay > 999999) then delay := 999999; end if;
+            if ((JB /= preposition) and delay > 325000) then -- 325000 is delay to compensate for rebounce
+              case((preposition & JB)) is
+                  when "0111" =>
+                      position <= position + 1;
+                  when "1110" =>
+                      position <= position + 1;
+                  when "1000" =>
+                      position <= position + 1;
+                  when "0001" =>
+                      position <= position + 1;
+                  when "0100" =>
+                      position <= position - 1;
+                  when "0010" =>
+                      position <= position - 1;
+                  when "1011" =>
+                      position <= position - 1;
+                  when "1101" =>
+                      position <= position - 1;
+                  when others =>
+                      position <= position;
+               end case;
+               delay  := 0;
+            end if;
+            preposition <= JB;
+            led <= std_logic_vector(position);
+              
+            if(btnC = '1') then
+              position <= (others => '0');
+            end if; 
+        end if;
+
     end process;
 end Behavioral;
